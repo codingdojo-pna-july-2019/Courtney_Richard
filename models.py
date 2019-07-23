77d0785126daa723f38e522ea53867ce71976ba2
Courtney Staunton <tealstaunto@gmail.com>
2019-07-22 18:09:04 -0700

    added one to many relationship b/w classes user and events, also added unique and nullable to different columns

--- models.py
+++ models.py
@@ -6,13 +6,14 @@ from config import db
 class User(db.Model):
     __Tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    password = db.Column(db.String(200))
-    email = db.Column(db.String(100))
+    first_name = db.Column(db.String(100), nullable = False)
+    last_name = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(200), unique = True, nullable = False)
+    email = db.Column(db.String(100), unique = True, nullable = False)
     security_answer1 = db.Column(db.String(200))
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+    events = db.relationship ('Event', backref = 'user', lazy = True)
 
 class Event(db.Model):
     __Tablename__ = "events"
@@ -22,3 +23,4 @@ class Event(db.Model):
     time = db.Column(db.TIME)
     location = db.Column(db.String(100))
     information = db.Column(db.TEXT)
+    user_id = db.Column(db.Interger, db.ForeignKey('user.id'), nullable = False)
